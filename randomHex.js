634bf9145cad9fb00a836cabda282df1236644ce
nextgtrgod <KYakovlev7@gmail.com>
2020-04-30 18:52:06 +0300

    another fix

--- randomHex.js
+++ randomHex.js
@@ -1,4 +1,4 @@
 
-let randomHex = () => '#' + (~~(Math.random() * 256**3 - 1)).toString(16)
+let randomHex = () => '#' + (~~(Math.random() * 256**3 - 1)).toString(16).padEnd(6, '0')
 
 module.exports = randomHex
